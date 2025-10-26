c591559c9946ca9b9fd9d20b3592bcada635125d
Monique <107360758+pursuit-monique@users.noreply.github.com>
2025-10-26 14:07:41 -0500

    First push of code

--- db/userModel.js
+++ db/userModel.js
@@ -6,7 +6,7 @@ const UserSchema = new mongoose.Schema({
   email: {
     type: String,
     required: [true, "Please provide an Email!"],
-    unique: [true, "Email Exist"],
+    unique: [true, "Email Exists"],
   },
 
   //   password field
